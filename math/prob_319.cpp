6b88448da0ee6fb47ca5be599e7ae319aa4a3f10
Chanpreet Singh <schanpreetsingh2001@gmail.com>
2024-07-12 23:12:14 +0530

    Added solution for Bulb Switcher

---
+++ math/prob_319.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return floor(sqrt(n));
+    }
+};
+
+//man I am forgetting my own methods now. But there was one plus point in this one. Opened the discussion tab(which I had never opened prior) and I saw 
+//saw someone comment that whenever you see such questions, wherein you got no fucking clue, just write down the cases all by hand and try to see a pattern
+//So I wrote them down and kinda could see something. Whereas the yesterday's me would have straight up went to the solution. So yeah, I think I have 
+//hopefully moved in the direction of regaining my skill atleast. (Still can't deny the fact that I am still piss low on confidence and hence why 
+//I am not attempting any contests rn, cuz even if I knew a solution, the mental barrier right now is huge)
